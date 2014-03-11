cd70f6b00279b1e49feea4ae4af2a689b1255359
Dong Fang <yp.fangdong@gmail.com>
2014-03-12 00:29:46 +0800

    ...

--- src/sdk/c/pio.h
+++ src/sdk/c/pio.h
@@ -1,11 +1,15 @@
 #ifndef _HPIO_CAPI_
 #define _HPIO_CAPI_
 
+#define GRPNAME_MAX 128
 
+typedef void * pio_producer_t;
+typedef void * pio_comsumer_t;
 
-
-
-
+pio_producer_t producer_new(const char *addr, const char grp[GRPNAME_MAX]);
+void producer_destroy(pio_producer_t pp);
+pio_comsumer_t comsumer_new(const char *addr, const char grp[GRPNAME_MAX]);
+void comsumer_destroy(pio_comsumer_t pc);
 
 
 
