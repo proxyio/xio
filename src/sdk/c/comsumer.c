b1860261eb46eb52b8a58788f984792004da11c6
remaper <yp.fangdong@gmail.com>
2014-03-13 11:19:20 +0800

    do while {}

--- src/sdk/c/comsumer.c
+++ src/sdk/c/comsumer.c
@@ -26,45 +26,36 @@ void comsumer_destroy(comsumer_t *pp) {
 
 int comsumer_recv_request(comsumer_t *pp, char **data, int64_t *size,
 			  char **rt, int64_t *rt_size) {
-    int ret, hready = false;
     struct pio_hdr h = {};    
     proxyio_t *io = container_of(pp, proxyio_t, sockfd);
     struct bio *b = &io->b;
-    
-    do {
-	ret = bio_prefetch(b, &io->sock_ops);
-	if (!hready && b->bsize >= sizeof(h)) {
-	    hready = true;
-	    bio_copy(b, (char *)&h, sizeof(h));
-	    if (!ph_validate(&h)) {
-		errno = PIO_ECHKSUM;
-		return -1;
-	    }
-	}
-    } while (ret < 0 && errno != EAGAIN &&
-	     (b->bsize < sizeof(h) || b->bsize < pio_pkg_size(&h)));
-    if (hready && b->bsize >= pio_pkg_size(&h)) {
-	if (!(*data = (char *)mem_zalloc(h.size)))
-	    return -1;
-	if (!(*rt = (char *)mem_zalloc(sizeof(h) + pio_rt_size(&h)))) {
-	    *data = NULL;
-	    mem_free(*data, h.size);
-	    return -1;
-	}
-	*size = h.size;
-	*rt_size = pio_rt_size(&h);
-	bio_read(b, *rt, sizeof(h));
-	bio_read(b, *data, h.size);
-	bio_read(b, (*rt) + sizeof(h), *rt_size);
-	return 0;
+
+    if ((b->bsize >= sizeof(h)) && !({
+		bio_copy(b, (char *)&h, sizeof(h)); ph_validate(&h);})) {
+	errno = PIO_ECHKSUM;
+	return -1;
     }
-    return ret >= 0 ? 0 : -1;
+    while (b->bsize < sizeof(h) || b->bsize < pio_pkg_size(&h))
+	if (bio_prefetch(b, &io->sock_ops) < 0 && errno != EAGAIN)
+	    return -1;
+    if (!(*data = (char *)mem_zalloc(h.size)))
+	return -1;
+    if (!(*rt = (char *)mem_zalloc(sizeof(h) + pio_rt_size(&h)))) {
+	*data = NULL;
+	mem_free(*data, h.size);
+	return -1;
+    }
+    *size = h.size;
+    *rt_size = pio_rt_size(&h);
+    bio_read(b, *rt, sizeof(h));
+    bio_read(b, *data, h.size);
+    bio_read(b, (*rt) + sizeof(h), *rt_size);
+    return 0;
 }
 
 
 int comsumer_send_response(comsumer_t *pp, const char *data, int64_t size,
 			   const char *_rt, int64_t rt_size) {
-    int ret;
     proxyio_t *io = container_of(pp, proxyio_t, sockfd);
     struct pio_hdr *h = (struct pio_hdr *)_rt;
     struct pio_rt *rt = (struct pio_rt *)(_rt + sizeof(h));
@@ -79,10 +70,9 @@ int comsumer_send_response(comsumer_t *pp, const char *data, int64_t size,
     BUG_ON(bio_write(&io->b, (char *)h, sizeof(h)) != sizeof(h));
     BUG_ON(bio_write(&io->b, data, size) != size);
     BUG_ON(bio_write(&io->b, (char *)rt, pio_rt_size(h)) != pio_rt_size(h));
-    while (!bio_empty(&io->b)) {
-	if ((ret = bio_flush(&io->b, &io->sock_ops)) < 0 && errno != EAGAIN)
-	    break;
-    }
-    return ret >= 0 ? 0 : -1;
+    while (!bio_empty(&io->b))
+	if (bio_flush(&io->b, &io->sock_ops) < 0 && errno != EAGAIN)
+	    return -1;
+    return 0;
 }
 
--- src/sdk/c/producer.c
+++ src/sdk/c/producer.c
@@ -24,7 +24,6 @@ void producer_destroy(producer_t *pp) {
 
 
 int producer_send_request(producer_t *pp, const char *data, int64_t size) {
-    int ret;
     proxyio_t *io = container_of(pp, proxyio_t, sockfd);
     struct pio_rt rt = {
 	.cost = 0,
@@ -45,41 +44,32 @@ int producer_send_request(producer_t *pp, const char *data, int64_t size) {
     BUG_ON(bio_write(&io->b, (char *)&h, sizeof(h)) != sizeof(h));
     BUG_ON(bio_write(&io->b, data, size) != size);
     BUG_ON(bio_write(&io->b, (char *)&rt, sizeof(rt)) != sizeof(rt));
-    while (!bio_empty(&io->b)) {
-	if ((ret = bio_flush(&io->b, &io->sock_ops)) < 0 && errno != EAGAIN)
-	    break;
-    }
-    return ret >= 0 ? 0 : -1;
+    while (!bio_empty(&io->b))
+	if (bio_flush(&io->b, &io->sock_ops) < 0 && errno != EAGAIN)
+	    return -1;
+    return 0;
 }
 
 
 int producer_recv_response(producer_t *pp, char **data, int64_t *size) {
-    int ret, hready = false;
     struct pio_rt rt;
     struct pio_hdr h = {};    
     proxyio_t *io = container_of(pp, proxyio_t, sockfd);
     struct bio *b = &io->b;
-    
-    do {
-	ret = bio_prefetch(b, &io->sock_ops);
-	if (!hready && b->bsize >= sizeof(h)) {
-	    hready = true;
-	    bio_copy(b, (char *)&h, sizeof(h));
-	    if (!ph_validate(&h)) {
-		errno = PIO_ECHKSUM;
-		return -1;
-	    }
-	}
-    } while (ret < 0 && errno != EAGAIN &&
-	     (b->bsize < sizeof(h) || b->bsize < pio_pkg_size(&h)));
-    if (hready && b->bsize >= pio_pkg_size(&h)) {
-	if (!(*data = (char *)mem_zalloc(h.size)))
-	    return -1;
-	*size = h.size;
-	bio_read(b, (char *)&h, sizeof(h));
-	bio_read(b, *data, h.size);
-	bio_read(b, (char *)&rt, sizeof(rt));
-	return 0;
+
+    if ((b->bsize >= sizeof(h)) && !({
+		bio_copy(b, (char *)&h, sizeof(h)); ph_validate(&h);})) {
+	errno = PIO_ECHKSUM;
+	return -1;
     }
-    return ret >= 0 ? 0 : -1;
+    while (b->bsize < sizeof(h) || b->bsize < pio_pkg_size(&h))
+	if (bio_prefetch(b, &io->sock_ops) < 0 && errno != EAGAIN)
+	    return -1;
+    if (!(*data = (char *)mem_zalloc(h.size)))
+	return -1;
+    *size = h.size;
+    bio_read(b, (char *)&h, sizeof(h));
+    bio_read(b, *data, h.size);
+    bio_read(b, (char *)&rt, sizeof(rt));
+    return 0;
 }
