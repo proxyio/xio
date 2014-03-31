1afd9060fb6eb77668a16ad5f3abe4542c297e9f
remaper <yp.fangdong@gmail.com>
2014-03-31 19:02:29 +0800

    [feature] remove unneed extern function

--- src/base/channel/io_channel.c
+++ src/base/channel/io_channel.c
@@ -11,24 +11,13 @@ extern struct channel_global cn_global;
 extern uint32_t channel_msgiov_len(struct channel_msg *msg);
 extern char *channel_msgiov_base(struct channel_msg *msg);
 
-extern int global_get_channel_id();
-extern void global_put_channel_id(int cd);
 extern struct channel *global_channel(int cd);
-
-extern int global_get_poller_id();
-extern void global_put_poller_id(int pd);
-extern epoll_t *global_poller(int pd);
-extern int select_a_poller(int cd);
-
 extern void global_put_closing_channel(struct channel *cn);
-extern struct channel *global_get_closing_channel(int pd);
 
 extern void global_channel_poll_state(struct channel *cn);
 
-extern void generic_channel_init(int cd, int isaccepter, int fd, struct transport *tp);
-extern void generic_channel_destroy(struct channel *cn);
-
-extern struct channel_msg *channel_allocmsg(uint32_t payload_sz, uint32_t control_sz);
+extern struct channel_msg *channel_allocmsg(uint32_t payload_sz,
+    uint32_t control_sz);
 extern void channel_freemsg(struct channel_msg *msg);
 
 
@@ -76,31 +65,17 @@ static void io_channel_close(int cd) {
 static int io_channel_setopt(int cd, int opt, void *val, int valsz) {
     int rc = 0;
     struct channel *cn = global_channel(cd);
-    struct transport *tp = cn->tp;
 
     mutex_lock(&cn->lock);
-
-    if (opt == TP_NOBLOCK) {
-	if (!cn->fasync && (*(int *)val))
-	    cn->fasync = true;
-	else if (cn->fasync && (*(int *)val) == 0)
-	    cn->fasync = false;
-    } else
-	rc = tp->setopt(cn->fd, opt, val, valsz);
-
     mutex_unlock(&cn->lock);
     return rc;
 }
 
 static int io_channel_getopt(int cd, int opt, void *val, int valsz) {
-    int rc;
+    int rc = 0;
     struct channel *cn = global_channel(cd);
-    struct transport *tp = cn->tp;
 
     mutex_lock(&cn->lock);
-
-    rc = tp->getopt(cn->fd, opt, val, valsz);
-
     mutex_unlock(&cn->lock);
     return rc;
 }
