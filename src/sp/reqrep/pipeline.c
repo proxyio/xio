302e01f108a79971e5ad1aa4d02bb8cf4d5f19ff
remaper <yp.fangdong@gmail.com>
2014-05-07 22:06:00 +0800

    [feature] receiver_add for pipeline pattern

--- src/sp/reqrep/pipeline.c
+++ src/sp/reqrep/pipeline.c
@@ -21,3 +21,69 @@
 */
 
 #include "req_ep.h"
+#include "rep_ep.h"
+
+static void double_lock(struct epbase *ep1, struct epbase *ep2) {
+    mutex_lock(&ep1->lock);
+    mutex_lock(&ep2->lock);
+}
+
+static void double_unlock(struct epbase *ep1, struct epbase *ep2) {
+    mutex_lock(&ep2->lock);
+    mutex_lock(&ep1->lock);
+}
+
+static int receiver_add(struct epbase *ep, struct epsk *sk, char *ubuf) {
+    struct xmsg *msg = cont_of(ubuf, struct xmsg, vec.chunk);
+    struct spr *r = rt_cur(ubuf);
+    struct epbase *peer = &(cont_of(ep, struct rep_ep, base)->peer)->base;
+
+    if (memcmp(r->uuid, sk->uuid, sizeof(sk->uuid)) != 0) {
+	uuid_copy(sk->uuid, r->uuid);
+    }
+    list_add_tail(&msg->item, &peer->snd.head);
+    peer->snd.size += xmsglen(ubuf);
+    return 0;
+}
+
+static int receiver_rm(struct epbase *ep, struct epsk *sk, char **ubuf) {
+    return 0;
+}
+
+static int dispatcher_add(struct epbase *ep, struct epsk *sk, char *ubuf) {
+    return 0;
+}
+
+static int dispatcher_rm(struct epbase *ep, struct epsk *sk, char **ubuf) {
+    return 0;
+}
+
+
+int epbase_pipeline(struct epbase *rep_ep, struct epbase *req_ep) {
+    struct rep_ep *frontend = cont_of(rep_ep, struct rep_ep, base);
+    struct req_ep *backend = cont_of(req_ep, struct req_ep, base);
+
+    double_lock(rep_ep, req_ep);
+    if (!list_empty(&rep_ep->connectors) || !list_empty(&rep_ep->bad_socks)) {
+	errno = EINVAL;
+	double_unlock(rep_ep, req_ep);
+	return -1;
+    }
+    if (!list_empty(&req_ep->connectors) || !list_empty(&req_ep->bad_socks)) {
+	errno = EINVAL;
+	double_unlock(rep_ep, req_ep);
+	return -1;
+    }
+    frontend->peer = backend;
+    backend->peer = frontend;
+    atomic_inc(&rep_ep->ref);
+    atomic_inc(&req_ep->ref);
+
+    rep_ep->vfptr.add = receiver_add;
+    rep_ep->vfptr.rm = receiver_rm;
+    req_ep->vfptr.add = dispatcher_add;
+    req_ep->vfptr.rm = dispatcher_rm;
+
+    double_unlock(rep_ep, req_ep);
+    return 0;
+}
