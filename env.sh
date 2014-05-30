fc757a5178b95d17cc2d05e8f8b5c03914d5c2e0
fangdong <yp.fangdong@gmail.com>
2014-05-31 02:19:20 +0800

    [feature] aotoreconf tools

--- env.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-
-export XIO_SOURCE_PATH=`pwd`
--- hudson.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-rm ./build -rf
-mkdir build
-cd ./build
-cmake .. && make && ./bin/unit_test
-lcov -d ../ -c -o pio.cov && genhtml -o cov pio.cov
