a557baa7c86805ad866c40f8be383a3a269049fa
fangdong <yp.fangdong@gmail.com>
2014-06-03 23:34:33 +0800

    [feature] ruby extension for xio

--- binding/ruby/xio_rbif.c
+++ binding/ruby/xio_rbif.c
@@ -1,3 +1,25 @@
+/*
+  Copyright (c) 2013-2014 Dong Fang. All rights reserved.
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"),
+  to deal in the Software without restriction, including without limitation
+  the rights to use, copy, modify, merge, publish, distribute, sublicense,
+  and/or sell copies of the Software, and to permit persons to whom
+  the Software is furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included
+  in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+  THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+  FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+  IN THE SOFTWARE.
+*/
+
 #include <xio/socket.h>
 #include <xio/poll.h>
 #include <xio/sp.h>
@@ -8,7 +30,7 @@
 /* Defining a space for information and references about the module to be
  * stored internally
  */
-static VALUE _module = Qnil;
+
 
 static VALUE rb_xallocubuf(VALUE self, VALUE rb_str) {
     char *ubuf = xallocubuf(RSTRING(rb_str)->len);
@@ -215,7 +237,6 @@ void Init_xio() {
     int i;
     struct xsymbol *sb;
 
-    _module = rb_define_module("xio");
     for (i = 0; i < NELEM(const_symbols, struct xsymbol); i++) {
 	sb = &const_symbols[i];
 	rb_define_global_const(sb->name, INT2FIX(sb->value));
