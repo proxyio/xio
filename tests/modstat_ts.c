b09955a2ff0d88c2053efddc5171bc402a360f9d
fangdong <yp.fangdong@gmail.com>
2014-06-15 11:19:00 +0800

    [feature] mstats

--- tests/modstat_ts.c
+++ tests/modstat_ts.c
@@ -1,15 +1,15 @@
-#include <utils/modstat.h>
+#include <utils/mstats.h>
 
-static void s_warn (modstat_t *self, int sl, int key, int64_t ts, int64_t val)
+static void s_warn (mstats_t *self, int sl, int key, int64_t ts, int64_t val)
 {
 }
-static void m_warn (modstat_t *self, int sl, int key, int64_t ts, int64_t val)
+static void m_warn (mstats_t *self, int sl, int key, int64_t ts, int64_t val)
 {
 }
-static void h_warn (modstat_t *self, int sl, int key, int64_t ts, int64_t val)
+static void h_warn (mstats_t *self, int sl, int key, int64_t ts, int64_t val)
 {
 }
-static void d_warn (modstat_t *self, int sl, int key, int64_t ts, int64_t val)
+static void d_warn (mstats_t *self, int sl, int key, int64_t ts, int64_t val)
 {
 }
 
@@ -19,32 +19,32 @@ enum {
     UT_KEYRANGE,
 };
 
-DEFINE_MODSTAT (ut, UT_KEYRANGE);
+DEFINE_MSTATS (ut, UT_KEYRANGE);
 
 
-static int modstat_test()
+static int mstats_test()
 {
 	int i;
-	ut_modstat_t utms = {};
-	modstat_t *ms = &utms.self;
+	ut_mstats_t utms = {};
+	mstats_t *ms = &utms.self;
 
-	INIT_MODSTAT (utms);
+	INIT_MSTATS (utms);
 	ms->slv[MSL_S] = 500;
 	ms->slv[MSL_M] = 1000;
 	ms->slv[MSL_H] = 2000;
 	ms->slv[MSL_D] = 4000;
-	modstat_set_warnf (ms, MSL_S, s_warn);
-	modstat_set_warnf (ms, MSL_M, m_warn);
-	modstat_set_warnf (ms, MSL_H, h_warn);
-	modstat_set_warnf (ms, MSL_D, d_warn);
-	modstat_set_threshold (ms, MSL_S, SEND, 100);
-	modstat_set_threshold (ms, MSL_M, SEND, 200);
-	modstat_set_threshold (ms, MSL_H, SEND, 300);
-	modstat_set_threshold (ms, MSL_D, SEND, 500);
+	mstats_set_warnf (ms, MSL_S, s_warn);
+	mstats_set_warnf (ms, MSL_M, m_warn);
+	mstats_set_warnf (ms, MSL_H, h_warn);
+	mstats_set_warnf (ms, MSL_D, d_warn);
+	mstats_set_threshold (ms, MSL_S, SEND, 100);
+	mstats_set_threshold (ms, MSL_M, SEND, 200);
+	mstats_set_threshold (ms, MSL_H, SEND, 300);
+	mstats_set_threshold (ms, MSL_D, SEND, 500);
 	for (i = 0; i < 1000; i++) {
-		modstat_incrkey (ms, SEND);
-		modstat_incrkey (ms, RECV);
-		modstat_update_timestamp (ms, gettimeof (ms) );
+		mstats_incrkey (ms, SEND);
+		mstats_incrkey (ms, RECV);
+		mstats_update_timestamp (ms, gettimeof (ms) );
 	}
 	return 0;
 }
@@ -52,10 +52,10 @@ static int modstat_test()
 
 static int gtip (const char *str, const char *item, int *tr, int *v)
 {
-	return generic_parse_modstat_item (str, item, tr, v);
+	return generic_parse_mstats_item (str, item, tr, v);
 }
 
-static int modstat_item_parse_test()
+static int mstats_item_parse_test()
 {
 	int tr = 0;
 	int v = 0;
@@ -87,7 +87,7 @@ static int modstat_item_parse_test()
 
 int main (int argc, char **argv)
 {
-	modstat_test();
-	modstat_item_parse_test();
+	mstats_test();
+	mstats_item_parse_test();
 	return 0;
 }
