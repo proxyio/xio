ebab9e8f15768c0030d480acac127540cb33f9c0
fangdong <yp.fangdong@gmail.com>
2014-07-01 11:47:54 +0800

    [feature] remove dead code

--- binding/lua/lua_xio.c
+++ binding/lua/lua_xio.c
@@ -29,109 +29,6 @@
 #include <lauxlib.h>
 #include <utils/base.h>
 
-struct sym_kv {
-	const char name[32];
-	int value;
-};
-
-static int lua_ubuf_alloc (lua_State *L)
-{
-	const char *str = luaL_checkstring (L, 1);
-	char *ubuf = ubuf_alloc (strlen (str) );
-	strncpy (ubuf, str, strlen (ubuf) );
-	lua_pushlightuserdata (L, ubuf);
-	return 1;
-}
-
-static int lua_ubuf_len (lua_State *L)
-{
-	char *ubuf = (char *) lua_touserdata (L, 1);
-	luaL_argcheck (L, ubuf != NULL, 1, "`ubuf' expected");
-	lua_pushnumber (L, ubuf_len (ubuf) );
-	return 1;
-}
-
-static int lua_ubuf_free (lua_State *L)
-{
-	char *ubuf = (char *) lua_touserdata (L, 1);
-	ubuf_free (ubuf);
-	return 1;
-}
-
-static int lua_xsocket (lua_State *L)
-{
-	int pf = luaL_checkint (L, 1);
-	int socktype = luaL_checkint (L, 2);
-	int fd = xsocket (pf, socktype);
-	lua_pushnumber (L, fd);
-	return 1;
-}
-
-static int lua_xbind (lua_State *L)
-{
-	int fd = luaL_checkint (L, 1);
-	const char *addr = luaL_checkstring (L, 2);
-	lua_pushnumber (L, xbind (fd, addr) );
-	return 1;
-}
-
-static int lua_xlisten (lua_State *L)
-{
-	const char *addr = luaL_checkstring (L, 1);
-	lua_pushnumber (L, xlisten (addr) );
-	return 1;
-}
-
-static int lua_xaccept (lua_State *L)
-{
-	int fd = luaL_checkint (L, 1);
-	lua_pushnumber (L, xaccept (fd) );
-	return 1;
-}
-
-static int lua_xconnect (lua_State *L)
-{
-	const char *addr = luaL_checkstring (L, 1);
-	lua_pushnumber (L, xconnect (addr) );
-	return 1;
-}
-
-static int lua_xrecv (lua_State *L)
-{
-	char *ubuf = 0;
-	int fd = luaL_checkint (L, 1);
-	lua_pushnumber (L, xrecv (fd, &ubuf) );
-	lua_pushlightuserdata (L, ubuf);
-	return 2;
-}
-
-static int lua_xsend (lua_State *L)
-{
-	int fd = luaL_checkint (L, 1);
-	char *ubuf = (char *) lua_touserdata (L, 2);
-	lua_pushnumber (L, xsend (fd, ubuf) );
-	return 1;
-}
-
-static int lua_xclose (lua_State *L)
-{
-	int fd = luaL_checkint (L, 1);
-	lua_pushnumber (L, xclose (fd) );
-	return 1;
-}
-
-static int lua_xsetopt (lua_State *L)
-{
-	return 0;
-}
-
-static int lua_xgetopt (lua_State *L)
-{
-	return 0;
-}
-
-
-
 static int lua_sp_endpoint (lua_State *L)
 {
 	int sp_family = luaL_checkint (L, 1);
@@ -209,21 +106,7 @@ static int lua_sp_listen (lua_State *L)
 }
 
 
-static const struct luaL_reg api_symbols [] = {
-	{"ubuf_alloc",      lua_ubuf_alloc},
-	{"ubuf_len",        lua_ubuf_len},
-	{"ubuf_free",       lua_ubuf_free},
-	{"xsocket",         lua_xsocket},
-	{"xbind",           lua_xbind},
-	{"xlisten",         lua_xlisten},
-	{"xconnect",        lua_xconnect},
-	{"xaccept",         lua_xaccept},
-	{"xrecv",           lua_xrecv},
-	{"xsend",           lua_xsend},
-	{"xclose",          lua_xclose},
-	{"xsetopt",         lua_xsetopt},
-	{"xgetopt",         lua_xgetopt},
-
+static const struct luaL_reg func_symbols [] = {
 	{"sp_endpoint",     lua_sp_endpoint},
 	{"sp_close",        lua_sp_close},
 	{"sp_send",         lua_sp_send},
@@ -237,35 +120,12 @@ static const struct luaL_reg api_symbols [] = {
 };
 
 
+struct sym_kv {
+	const char name[32];
+	int value;
+};
+
 static const struct sym_kv const_symbols [] = {
-	{"XPF_TCP",      XPF_TCP},
-	{"XPF_IPC",      XPF_IPC},
-	{"XPF_INPROC",   XPF_INPROC},
-	{"XLISTENER",    XLISTENER},
-	{"XCONNECTOR",   XCONNECTOR},
-	{"XSOCKADDRLEN", XSOCKADDRLEN},
-
-	{"XL_SOCKET",    XL_SOCKET},
-	{"XNOBLOCK",     XNOBLOCK},
-	{"XSNDWIN",      XSNDWIN},
-	{"XRCVWIN",      XRCVWIN},
-	{"XSNDBUF",      XSNDBUF},
-	{"XRCVBUF",      XRCVBUF},
-	{"XLINGER",      XLINGER},
-	{"XSNDTIMEO",    XSNDTIMEO},
-	{"XRCVTIMEO",    XRCVTIMEO},
-	{"XRECONNECT",   XRECONNECT},
-	{"XSOCKTYPE",    XSOCKTYPE},
-	{"XSOCKPROTO",   XSOCKPROTO},
-	{"XTRACEDEBUG",  XTRACEDEBUG},
-
-	{"XPOLLIN",      XPOLLIN},
-	{"XPOLLOUT",     XPOLLOUT},
-	{"XPOLLERR",     XPOLLERR},
-	{"XPOLL_ADD",    XPOLL_ADD},
-	{"XPOLL_DEL",    XPOLL_DEL},
-	{"XPOLL_MOD",    XPOLL_MOD},
-
 	{"SP_REQREP",    SP_REQREP},
 	{"SP_BUS",       SP_BUS},
 	{"SP_PUBSUB",    SP_PUBSUB},
@@ -274,22 +134,16 @@ static const struct sym_kv const_symbols [] = {
 	{"SP_PROXY",     SP_PROXY},
 };
 
-#define LUA_REGISTER_GLOBAL_FUNCTION(L, reg) do {	\
-	lua_pushcfunction(L, (reg).func);		\
-	lua_setglobal(L, (reg).name);			\
-    } while (0)
-
-#define LUA_REGISTER_GLOBAL_CONSTANT(L, cst) do {	\
-	lua_pushnumber(L, (cst).value );		\
-	lua_setglobal(L, (cst).name );			\
-    } while (0)
-
 LUA_API int luaopen_xio (lua_State *L)
 {
 	int i;
-	for (i = 0; i < NELEM (api_symbols, struct luaL_reg); i++)
-		LUA_REGISTER_GLOBAL_FUNCTION (L, api_symbols[i]);
-	for (i = 0; i < NELEM (const_symbols, struct sym_kv); i++)
-		LUA_REGISTER_GLOBAL_CONSTANT (L, const_symbols[i]);
+	for (i = 0; i < NELEM (func_symbols, struct luaL_reg); i++) {
+		lua_pushcfunction (L, func_symbols[i].func);
+		lua_setglobal (L, func_symbols[i].name);
+	}
+	for (i = 0; i < NELEM (const_symbols, struct sym_kv); i++) {
+		lua_pushnumber (L, const_symbols[i].value);
+		lua_setglobal (L, const_symbols[i].name);
+	}
 	return 0;
 }
