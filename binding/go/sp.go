69d70e2f314ccdd45199d06fc0f8783751c2a146
fangdong <yp.fangdong@gmail.com>
2014-07-03 00:49:09 +0800

    [feature] reimpl go_sp_send () and go_sp_recv () API

--- binding/go/sp.go
+++ binding/go/sp.go
@@ -4,11 +4,14 @@ package xio
  #cgo CFLAGS: -I/usr/local/include -I/usr/include
  #cgo LDFLAGS: -L/usr/local/lib -L/usr/lib -L/usr/local/lib64 -L/usr/lib64 -lxio -luuid
 
+ #include <string.h>
  #include <xio/sp.h>
  #include <xio/sp_reqrep.h>
-
+ 
 */
 import "C"
+import "unsafe"
+
 
 const (
 	SP_REQREP = C.SP_REQREP
@@ -19,42 +22,77 @@ const (
 	SP_PROXY  = C.SP_PROXY
 )
 
-func sp_endpoint(sp_family int, sp_type int) (eid int) {
-	eid = int(C.sp_endpoint(C.int(sp_family), C.int(sp_type)))
+type Msg struct {
+	data   []byte
+	hdr    [][]byte
+}
+
+func sp_endpoint (sp_family int, sp_type int) (eid int) {
+	eid = int (C.sp_endpoint (C.int (sp_family), C.int (sp_type)))
 	return eid
 }
 
-func sp_close(eid int) (rc int) {
-	rc = int(C.sp_close(C.int(eid)))
+func sp_close (eid int) (rc int) {
+	rc = int (C.sp_close (C.int (eid)))
 	return rc
 }
 
-func sp_recv(eid int) (rc int, ubuf *C.char) {
-	rc = int(C.sp_recv(C.int(eid), &ubuf))
-	return rc, ubuf
+func sp_recv(eid int) (rc int, msg Msg) {
+	var (
+		ubuf *C.char
+		cur  *C.char
+		leng int
+	)
+
+	if rc = int (C.sp_recv (C.int (eid), &ubuf)); rc == 0 {
+		msg.data = C.GoBytes (unsafe.Pointer (ubuf), C.ubuf_len (ubuf))
+		cur = C.ubufctl_first (ubuf)
+		msg.hdr = append (msg.hdr, C.GoBytes (unsafe.Pointer (cur), C.ubuf_len (cur)))
+		for leng = int (C.ubufctl_num (ubuf)); leng > 0; leng-- {
+			cur = C.ubufctl_next (ubuf, cur)
+			msg.hdr = append (msg.hdr, C.GoBytes (unsafe.Pointer (cur), C.ubuf_len (cur)))
+		}
+	}
+	return rc, msg
 }
 
-func sp_send(eid int, ubuf *C.char) (rc int) {
-	rc = int(C.sp_send(C.int(eid), ubuf))
+func sp_send(eid int, msg Msg) (rc int) {
+	var (
+		ubuf *C.char
+		cur  *C.char
+	)
+
+	ubuf = C.ubuf_alloc (C.int(len(msg.data)))
+	C.memcpy (unsafe.Pointer(ubuf), unsafe.Pointer (&msg.data[0]), C.size_t (len (msg.data)))
+
+	for _, s := range msg.hdr {
+		cur = C.ubuf_alloc (C.int(len (s)))
+		C.memcpy (unsafe.Pointer(cur), unsafe.Pointer (&s[0]), C.size_t (len (s)))
+		C.ubufctl_add (ubuf, cur)
+	}
+	if rc = int (C.sp_send (C.int (eid), ubuf)); rc != 0 {
+		C.ubuf_free (ubuf)
+	}
 	return rc
 }
+
 
 func sp_add(eid int, sockfd int) (rc int) {
-	rc = int(C.sp_add(C.int(eid), C.int(sockfd)))
+	rc = int (C.sp_add (C.int (eid), C.int (sockfd)))
 	return rc
 }
 
 func sp_rm(eid int, sockfd int) (rc int) {
-	rc = int(C.sp_rm(C.int(eid), C.int(sockfd)))
+	rc = int (C.sp_rm (C.int (eid), C.int (sockfd)))
 	return rc
 }
 
 func sp_connect(eid int, sockaddr string) (rc int) {
-	rc = int(C.sp_connect(C.int(eid), C.CString(sockaddr)))
+	rc = int (C.sp_connect(C.int (eid), C.CString(sockaddr)))
 	return rc
 }
 
 func sp_listen(eid int, sockaddr string) (rc int) {
-	rc = int(C.sp_listen(C.int(eid), C.CString(sockaddr)))
+	rc = int (C.sp_listen (C.int (eid), C.CString (sockaddr)))
 	return rc
 }
--- binding/go/sp_test.go
+++ binding/go/sp_test.go
@@ -6,40 +6,41 @@ import (
 )
 
 func TestREQREP(t *testing.T) {
-	svr_eid := sp_endpoint(SP_REQREP, SP_REP)
-	cli_eid := sp_endpoint(SP_REQREP, SP_REQ)
+	recver := sp_endpoint(SP_REQREP, SP_REP)
+	sender := sp_endpoint(SP_REQREP, SP_REQ)
 	host := "inproc://go_reqrep"
 
 	for i := 1; i <= 10; i++ {
 		sockaddr := host + strconv.Itoa(i)
-		if rc := sp_listen(svr_eid, sockaddr); rc != 0 {
+		if rc := sp_listen(recver, sockaddr); rc != 0 {
 			t.Fatal()
 		}
 	}
 	for i := 1; i <= 10; i++ {
 		sockaddr := host + strconv.Itoa(i)
-		if rc := sp_connect(cli_eid, sockaddr); rc != 0 {
+		if rc := sp_connect(sender, sockaddr); rc != 0 {
 			t.Fatal()
 		}
 	}
 	for i := 1; i <= 10; i++ {
-		msg := xallocubuf(12)
-		if rc := sp_send(cli_eid, msg); rc != 0 {
+		var msg Msg
+		msg.data = []byte ("Hello world");
+		if rc := sp_send(sender, msg); rc != 0 {
 			t.Fatal()
 		}
 
-		if rc, msg := sp_recv(svr_eid); rc != 0 {
+		if rc, msg := sp_recv(recver); rc != 0 {
 			t.Fatal()
-		} else if rc = sp_send(svr_eid, msg); rc != 0 {
+		} else if rc = sp_send(recver, msg); rc != 0 {
 			t.Fatal()
 		}
 
-		if rc, msg := sp_recv(cli_eid); rc != 0 {
+		if rc, msg := sp_recv(sender); rc != 0 {
 			t.Fatal()
 		} else {
-			xfreeubuf(msg)
+			println (string(msg.data))
 		}
 	}
-	sp_close(svr_eid)
-	sp_close(cli_eid)
+	sp_close(recver)
+	sp_close(sender)
 }
