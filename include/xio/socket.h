ae029837cf287a32a005439db7c9181e28effc9e
fangdong <yp.fangdong@gmail.com>
2014-07-06 10:36:55 +0800

    [feature] remove XTRACEDEBUG option

--- include/xio/socket.h
+++ include/xio/socket.h
@@ -52,7 +52,6 @@ int xclose (int fd);
 #define XRECONNECT         8
 #define XSOCKTYPE          9
 #define XSOCKPROTO         10
-#define XTRACEDEBUG        11
 
 int xsetopt (int fd, int level, int opt, void *optval, int optlen);
 int xgetopt (int fd, int level, int opt, void *optval, int *optlen);
