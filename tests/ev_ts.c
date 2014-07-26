6e4e47c663cad13b21288780106f00441b195d63
fangdong <yp.fangdong@gmail.com>
2014-07-27 00:52:03 +0800

    [feature] fix ev_ts

--- tests/ev_ts.c
+++ tests/ev_ts.c
@@ -1,56 +1,58 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <ev/ev.h>
 
-static int size = 0;
+static int ev_in_size = 0;
+static int ev_out_size = 0;
 
-static void stdin_hndl (struct ev_fdset *evfds, struct ev_fd *evfd, int events)
+static void pipein_hndl (struct ev_fdset *evfds, struct ev_fd *evfd, int events)
 {
-	BUG_ON (events != 0);
-	size++;
+	BUG_ON (events != EV_READ);
+	ev_in_size++;
 }
 
-static void stdout_hndl (struct ev_fdset *evfds, struct ev_fd *evfd, int events)
+static void pipeout_hndl (struct ev_fdset *evfds, struct ev_fd *evfd, int events)
 {
 	BUG_ON (events != EV_WRITE);
-	size++;
-}
-
-static void stderr_hndl (struct ev_fdset *evfds, struct ev_fd *evfd, int events)
-{
-	BUG_ON (events != EV_WRITE);
-	size++;
+	ev_out_size++;
 }
 
 int main (int argc, char **argv)
 {
-	struct ev_fd ev_fd[3];
+	int pipefd[2];
+	struct ev_fd ev_fd[2];
 	struct ev_fdset fds;
 
+	BUG_ON (pipe (pipefd));
 	ev_fdset_init (&fds);
 	ev_fd_init (&ev_fd[0]);
 	ev_fd_init (&ev_fd[1]);
-	ev_fd_init (&ev_fd[2]);
 
-	ev_fd[0].fd = 0;
+	ev_fd[0].fd = pipefd[0];
 	ev_fd[0].events = EV_READ;
-	ev_fd[0].hndl = stdin_hndl;
+	ev_fd[0].hndl = pipein_hndl;
 
-	ev_fd[1].fd = 1;
-	ev_fd[1].events = EV_READ|EV_WRITE;
-	ev_fd[1].hndl = stdout_hndl;
-
-	ev_fd[2].fd = 2;
-	ev_fd[2].events = EV_READ|EV_WRITE;
-	ev_fd[2].hndl = stderr_hndl;
+	ev_fd[1].fd = pipefd[1];
+	ev_fd[1].events = EV_WRITE;
+	ev_fd[1].hndl = pipeout_hndl;
 
 	__ev_fdset_ctl (&fds, EV_ADD, &ev_fd[0]);
 	__ev_fdset_ctl (&fds, EV_ADD, &ev_fd[1]);
-	__ev_fdset_ctl (&fds, EV_ADD, &ev_fd[2]);
 
 	ev_fdset_poll (&fds, 10);
+	BUG_ON (ev_in_size != 0 || ev_out_size != 1);
+
+	BUG_ON (1 != write (pipefd[1], "1", 1));
+	ev_fdset_poll (&fds, 10);
+	BUG_ON (ev_in_size != 1 || ev_out_size != 2);
+
+	ev_fdset_poll (&fds, 10);
+	BUG_ON (ev_in_size != 2 || ev_out_size != 3);
+
 	__ev_fdset_ctl (&fds, EV_DEL, &ev_fd[0]);
 	ev_fdset_poll (&fds, 10);
-
+	BUG_ON (ev_in_size != 2 || ev_out_size != 4);
+	
 	ev_fdset_term (&fds);
 	return 0;
 }
