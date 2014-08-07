0f3d8cf34362d103f05585b537b57bf093c55534
fangdong <yp.fangdong@gmail.com>
2014-08-07 23:29:38 +0800

    [feature] multi sender

--- tools/lat/recver.c
+++ tools/lat/recver.c
@@ -12,7 +12,9 @@ int main (int argc, char **argv)
 	int eid;
 	int count = 0x7fffffff;
 	char *ubuf;
-
+	int qps;
+	int64_t st, lt;
+	
 	if (argc < 2) {
 		printf ("lat_recver tcp:127.0.0.1:1880 100000\n");
 		return 0;
@@ -22,9 +24,16 @@ int main (int argc, char **argv)
 	BUG_ON ((eid = sp_endpoint (SP_REQREP, SP_REP)) < 0);
 	BUG_ON (sp_listen (eid, argv[1]) < 0);
 
-	for (i = 0; i < count; i++) {
+	st = gettimeofms ();
+	for (i = 0, qps = 0; i < count; i++, qps++) {
 		BUG_ON (sp_recv (eid, &ubuf) != 0);
 		BUG_ON (sp_send (eid, ubuf));
+
+		if (i % 1000 == 0 && (lt = gettimeofms ()) - st > 1000) {
+			printf ("%d qps %d/s\n", getpid (), qps * 1000 / (lt - st));
+			st = lt;
+			qps = 0;
+		}
 	}
 	sp_close (eid);
 	return 0;
--- tools/lat/sender.c
+++ tools/lat/sender.c
@@ -7,7 +7,10 @@
 #include <utils/spinlock.h>
 #include <utils/thread.h>
 
-int main (int argc, char **argv)
+static int argc;
+static char **argv;
+
+int task_main (void *args)
 {
 	int s;
 	int i;
@@ -27,19 +30,35 @@ int main (int argc, char **argv)
 	if (argc > 3)
 		count = atoi (argv[3]);
 	BUG_ON ((eid = sp_endpoint (SP_REQREP, SP_REQ)) < 0);
-	for (i = 0; i < 8; i++)
+	for (i = 0; i < 1; i++)
 		BUG_ON (sp_connect (eid, argv[1]) < 0);
+
 	st = gettimeofms ();
-	for (i = 0; i < count; i++, qps++) {
+	for (i = 0, qps = 0; i < count; i++, qps++) {
 		BUG_ON (sp_send (eid, ubuf_alloc (size)) != 0);
 		BUG_ON (sp_recv (eid, &ubuf) != 0);
 		ubuf_free (ubuf);
-		if (count % 1000 == 0 && (lt = gettimeofms ()) - st > 1000) {
+		if (i % 1000 == 0 && (lt = gettimeofms ()) - st > 1000) {
+			printf ("%d qps %d/s\n", getpid (), qps * 1000 / (lt - st));
 			st = lt;
-			printf ("qps %d/s\n", qps);
 			qps = 0;
 		}
 	}
 	sp_close (eid);
+	return 0;
+}
+
+int main (int _argc, char **_argv)
+{
+	int i;
+	thread_t childs[2];
+
+	argc = _argc;
+	argv = _argv;
+	
+	for (i = 0; i < NELEM (childs, thread_t); i++)
+		thread_start (&childs[i], task_main, 0);
+	for (i = 0; i < NELEM (childs, thread_t); i++)
+		thread_stop (&childs[i]);
 	return 0;
 }
